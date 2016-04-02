038913bc65cff2bddc1a4c3151456b3c52a14bf3
Michael Krause <mekva6274@gmail.com>
2016-04-01 22:48:07 -0400

    updated archives

---
+++ archives/0.2a/demo/data.js
@@ -0,0 +1,3 @@
+var randData = new function(){
+	this.sampleData = {"hello": "Hello World!", "message": "Data binding in Quick.JS is easy and super powerful. Ready? Let's try something crazy: [[data.hello]]"};
+}
