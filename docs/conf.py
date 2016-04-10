dc00863f4d15ec2ce7791748c44df75a681e1ffa
Michael Krause <mekva6274@gmail.com>
2016-04-10 17:50:39 -0400

    more doc fixes

--- docs/conf.py
+++ docs/conf.py
@@ -209,7 +209,9 @@ htmlhelp_basename = 'QuickJSdoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
-latex_elements = {
+latex_elements = { 'classoptions': ',openany,oneside', 'babel' : '\\usepackage[english]{babel}' }
+#latex_elements = {
+
 # The paper size ('letterpaper' or 'a4paper').
 #'papersize': 'letterpaper',
 
@@ -221,7 +223,7 @@ latex_elements = {
 
 # Latex figure (float) alignment
 #'figure_align': 'htbp',
-}
+#}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
