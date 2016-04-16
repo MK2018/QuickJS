f35b1fdcc08b83c29e36824973ebc97c6167c84a
Michael Krause <mekva6274@gmail.com>
2016-04-16 18:28:30 -0400

    0.5.0b

---
+++ archives/0.5.0b/demo/data.js
@@ -0,0 +1,11 @@
+var randData = new function(){
+	this.dataSet1 = [
+	[ 1,  2,  3,  4],
+	[ 5,  6,  7,  8],
+	[ 9, 10, 11, 12],
+	[13, 14, 15, 16],
+	];
+	this.sampleData3 = {"test" : ""};
+	this.sampleData2 = {"firstline" : "this is the first line to test", "secondline" : "this is the second line to test!"};
+	this.sampleData = {"hello": "Hello World!", "message": "Data binding in Quick.JS is easy and super powerful. Ready? Let's try something crazy: [[data.hello]]"};
+}
